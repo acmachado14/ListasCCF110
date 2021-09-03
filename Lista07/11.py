119f7f49772524660f8dc86ba4a733aa349e6e7d
Angelo Machado <cupertinoangelo13@gmail.com>
2021-09-03 14:29:44 -0300

    Questao 11

---
+++ Lista07/11.py
@@ -0,0 +1,59 @@
+#11
+
+#a)
+A = []
+n = int(input())
+
+while n > 1000:
+    n = int(input())
+
+for i in range(n):
+    A.append(int(input()))
+
+for i in range(n):
+    aux = 0
+    for j in range(n-1):
+        if A[j] > A[j+1]:
+            aux = A[j]
+            A[j] = A[j+1]
+            A[j+1] = aux
+#b)
+
+for i in range(n):
+    repete = 0
+    for j in range(n):
+        if A[i] == A[j]:
+            repete += 1
+    print("O Numero", A[i] , "Se aparece", repete, " vezes")
+
+
+#c)
+
+for i in range(n):
+    for j in range(i+1, n):
+        if A[j] == A[i]:
+            for k in range(j, n-1):
+                A[k] = A[k + 1]
+            n -= 1
+        else:
+            j += 1
+
+    
+#d)
+
+for i in range(n):
+    print(A[i])
+
+while n > 1000:
+    n = int(input())
+
+for i in range(n):
+    A.append(int(input()))
+
+for i in range(n):
+    aux = 0
+    for j in range(n-1):
+        if A[j] > A[j+1]:
+            aux = A[j]
+            A[j] = A[j+1]
+            A[j+1] = aux
