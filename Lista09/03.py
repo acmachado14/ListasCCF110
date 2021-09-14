59718101c08514a733ee50335d41b207f1454fa8
Angelo Machado <cupertinoangelo13@gmail.com>
2021-09-13 22:13:26 -0300

    Questao 03

---
+++ Lista09/03.py
@@ -0,0 +1,13 @@
+#3. Crie um algoritmo que leia os elementos de uma matriz inteira 10 x 10 e escreva
+# o produto dos elementos que estão abaixo da diagonal principal.
+
+matriz = [[0 for j in range(10)] for i in range(10)]
+
+for i in range(len(matriz)):
+    for j in range(len(matriz[i])):
+        matriz[i][j] = int(input(f"Digite o valor para o índice ({i+1},{j+1}): "))
+
+for i in range(len(matriz)):
+    for j in range(len(matriz[i])):
+        if j < i:
+            print(matriz[i][j])
